6cf6c44c47b073dab314ef60604b626b7e03c7dc
Muskan-Garg19 <im.muskangarg@gmail.com>
2026-08-14 11:31:29 +0530

    Time: 0 ms (100%), Space: 43.3 MB (43.69%) - LeetHub

---
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        if (nums.length < 3) {
+            return 0;
+        }
+
+        int ans = 0;
+        int count = 0;
+        int diff = nums[1] - nums[0];
+
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] - nums[i - 1] == diff) {
+                count++;
+                ans += count;
+            } else {
+                diff = nums[i] - nums[i - 1];
+                count = 0;
+            }
+        }
+
+        return ans;
+    }
+}
