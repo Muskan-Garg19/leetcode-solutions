1fff695ccc25727c5b5ab14e43849bc59e0b57ed
Muskan-Garg19 <im.muskangarg@gmail.com>
2026-08-19 11:41:29 +0530

    Time: 20 ms (61.84%), Space: 53.5 MB (32.63%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -12,9 +12,7 @@ class Solution {
             map.put(row, arr);
         }
         int ans = (n - map.size()) * 2;
-        for(int key : map.keySet()) {
-            boolean[] arr = map.get(key);
-            int curr = 0;
+        for(boolean[] arr  : map.values()) {
             if(!arr[2] && !arr[3] && !arr[4] && !arr[5]) {
                 ans++;
                 if(!arr[6] && !arr[7] && !arr[8] && !arr[9]) {
