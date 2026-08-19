23cdc653738e57d884898dde0b1a8b04d7b98450
Muskan-Garg19 <im.muskangarg@gmail.com>
2026-08-19 11:39:22 +0530

    Time: 23 ms (52.11%), Space: 53 MB (45.53%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+        HashMap<Integer, boolean[]> map = new HashMap<>();
+        for(int i=0; i<reservedSeats.length; i++) {
+            int row = reservedSeats[i][0];
+            int col = reservedSeats[i][1];
+            boolean[] arr = new boolean[11];
+            if(map.containsKey(row)) {
+                arr = map.get(row);
+            }
+            arr[col] = true;
+            map.put(row, arr);
+        }
+        int ans = (n - map.size()) * 2;
+        for(int key : map.keySet()) {
+            boolean[] arr = map.get(key);
+            int curr = 0;
+            if(!arr[2] && !arr[3] && !arr[4] && !arr[5]) {
+                ans++;
+                if(!arr[6] && !arr[7] && !arr[8] && !arr[9]) {
+                    ans++;
+                }
+            }
+            else if(!arr[4] && !arr[5] && !arr[6] && !arr[7]) {
+                ans++;
+            }
+            else if(!arr[6] && !arr[7] && !arr[8] && !arr[9]) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
