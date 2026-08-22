377f8bdca71348b7f6ab0522d61f385da56a658f
Muskan-Garg19 <im.muskangarg@gmail.com>
2026-08-22 10:15:44 +0530

    Time: 1 ms (68.87%), Space: 43.7 MB (19.12%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,38 +1,31 @@
 class Solution {
-    private static int helper(int[] prices, int i, int buyStatus, int last, int[][][] dp) {
-        if(i == prices.length) {
+    private static int helper(int[] prices, int i, int buyStatus, int last, int[][] dp) {
+        if(i >= prices.length) {
             return 0;
         }
-        if(dp[i][buyStatus][last] != -1) {
-            return dp[i][buyStatus][last];
+        if(dp[i][buyStatus] != -1) {
+            return dp[i][buyStatus];
         }
         int ans = 0;
         if(buyStatus == 0) {
-            if(last != 1) {
-                int buy = helper(prices, i+1, 1, 0, dp) - prices[i];
-                int notBuy = helper(prices, i+1, 0, 0, dp);
-                ans = Math.max(buy, notBuy);
-            }
-            else {
-                ans = helper(prices, i+1, 0, 0, dp);
-            }
+            int buy = helper(prices, i+1, 1, 0, dp) - prices[i];
+            int notBuy = helper(prices, i+1, 0, 0, dp);
+            ans = Math.max(buy, notBuy);
         }
         else {
-            int sell = helper(prices, i+1, 0, 1, dp) + prices[i];
+            int sell = helper(prices, i+2, 0, 1, dp) + prices[i];
             int notSell = helper(prices, i+1, 1, 0, dp);
             ans = Math.max(sell, notSell);
         }
-        return dp[i][buyStatus][last] = ans;
+        return dp[i][buyStatus] = ans;
     }
 
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n][2][2];
+        int[][] dp = new int[n][2];
         for(int i=0; i<n; i++) {
             for(int j=0; j<2; j++) {
-                for(int k=0; k<2; k++) {
-                    dp[i][j][k] = -1;
-                }
+                dp[i][j] = -1;
             }
         }
 
