7baec699b20f65680fc2809c0007b41d0b98d004
Muskan-Garg19 <im.muskangarg@gmail.com>
2026-08-28 19:39:57 +0530

    Time: 2759 ms (5%), Space: 48.1 MB (8.38%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,10 +1,13 @@
 class Solution {
-    private static boolean isPalin(String s, int i, int j) {
+    private static boolean isPalin(String s, int i, int j, boolean[][] dp) {
         if(i > j) {
             return true;
         }
+        if(dp[i][j]) {
+            return true;
+        }
         if(s.charAt(i) == s.charAt(j)) {
-            return isPalin(s, i+1, j-1);
+            return isPalin(s, i+1, j-1, dp);
         }
         else {
             return false;
@@ -14,9 +17,11 @@ class Solution {
         int n = s.length();
         int longest = Integer.MIN_VALUE;
         int sp = -1;
+        boolean[][] dp = new boolean[n][n];
         for(int i=0; i<n; i++) {
             for(int j=i; j<n; j++) {
-                if(isPalin(s, i, j)) {
+                if(isPalin(s, i, j, dp)) {
+                    dp[i][j] = true;
                     if(j-i+1 > longest) {
                         longest = j-i+1;
                         sp = i;
