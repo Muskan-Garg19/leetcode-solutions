40ac26de30e2435c8bf4817a8aa3276a09a2a2c3
Muskan-Garg19 <im.muskangarg@gmail.com>
2026-09-03 21:01:30 +0530

    Time: 9 ms (34.78%), Space: 121.9 MB (30.44%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -0,0 +1,70 @@
+class Solution {
+    public boolean uniformArray(int[] nums) {
+        int n = nums.length;
+        int oddMin = Integer.MAX_VALUE;
+        int evenMin = Integer.MAX_VALUE;
+        for(int i=0; i<n; i++) {
+            if(nums[i] % 2 == 0) {
+                evenMin = Math.min(evenMin, nums[i]);
+            }
+            else {
+                oddMin = Math.min(oddMin, nums[i]);
+            }
+        }
+
+        boolean odd = true;
+        boolean even = true;
+        int i = 0;
+        while(i < n && odd && even) {
+            if(nums[i] % 2 != 0) {
+                odd = true;
+                if(oddMin != nums[i] && nums[i] - oddMin >= 1) {
+                    even = true;
+                }
+                else {
+                    even = false;
+                }
+            }
+            else {
+                even = true;
+                if(oddMin != nums[i] && nums[i] - oddMin >=1 ) {
+                    odd = true;
+                }
+                else {
+                    odd = false;
+                }
+            }
+            i++;
+        }
+
+        if(odd) {
+            while(i < n) {
+                if(nums[i] % 2 == 0) {
+                    if(oddMin != nums[i] && nums[i] - oddMin >= 1) {
+                        odd = true;
+                    }
+                    else {
+                        return false;
+                    }
+                }
+                i++;
+            }
+        }
+
+        else if(even) {
+            while(i < n) {
+                if(nums[i] % 2 != 0) {
+                    if(oddMin != nums[i] && nums[i] - oddMin >= 1) {
+                        even = true;
+                    }
+                    else {
+                        return false;
+                    }
+                }
+                i++;
+            }
+        }
+
+        return true;
+    }
+}
